4c0c4309b1c26afa247fb0db32a31b9a39b2e351
Chad Faurie <chad@codeworkshop.io>
2023-05-28 08:07:17 +0200

    fix(hooks): updated hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 
-import type { RootState, AppDispatch } from "./store";
+import type { AppDispatch } from "./store";
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector = useSelector<RootState>;
+export const useAppSelector = useSelector;
